c862bb7b462f4f64128ce2f6d508ebae77fd77c4
zamza1031 <zamza1031@gmail.com>
2026-05-13 14:32:15 -0500

    y

---
+++ index.js
@@ -0,0 +1,55 @@
+
+async function FetchData() {
+    try {
+        const PokemonName = document.getElementById("PokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${PokemonName}`);
+        if (!response.ok) {
+            throw new Error("could not fetch resource");
+        }
+        const data = await response.json();
+        const PokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("PokemonSprite");
+        const PokemonNameElement = document.getElementById("PokemonNameElement");
+        const PokemonDescription = document.getElementById("PokemonDescription");
+
+        PokemonNameElement.textContent = `Name: ${data.name}`;
+        PokemonDescription.textContent = `Description: ${data.description}`;
+        const PokemonID = document.getElementById("PokemonID");
+        PokemonID.textContent = `ID: ${data.id}`;
+        imgElement.src = PokemonSprite;
+        imgElement.alt = data.name;
+        imgElement.style.display = "block";
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function RandomPokemon() {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=100");
+        if (!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+        const data = await response.json();
+        const randomIndex = Math.floor(Math.random() * data.results.length);
+        const randomPokemon = data.results[randomIndex].name;
+
+        const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`);
+        if (!pokemonResponse.ok) {
+            throw new Error("Could not fetch Pokemon data");
+        }
+        const pokemonData = await pokemonResponse.json();
+        const PokemonNameElement = document.getElementById("PokemonNameElement");
+        const PokemonSprite = pokemonData.sprites.front_default;
+        const imgElement = document.getElementById("PokemonSprite");
+        const PokemonID = document.getElementById("PokemonID");
+
+        PokemonNameElement.textContent = `Name: ${pokemonData.name}`;
+        PokemonID.textContent = `ID: ${pokemonData.id}`;
+        imgElement.src = PokemonSprite;
+        imgElement.alt = pokemonData.name;
+        imgElement.style.display = "block";
+    } catch (error) {
+        console.error(error);
+    }
+}
